6f451a39245b67af86c9a885c453f55c278b4a33
zhouying <1770385837@qq.com>
2022-10-13 20:20:29 +0800

    Site updated: 2022-10-13 20:20:29

--- vvd_js/musicshow.js
+++ vvd_js/musicshow.js
@@ -1,7 +1,7 @@
 !(function() {
   function show() {
-    if(!isMobile()){
-       //如果是 PC 端
+    if(!isMobile() && !isWxClient()){
+       //如果不是移动端或微信环境
        if (location.href === "https://sweetying520.github.io/"){
             //简单判断首页，因为我的域名是https://sweetying520.github.io，所以首页就会触发
             //直接把音乐框隐藏
@@ -19,12 +19,12 @@
        		 })
        	}
     }else{
-        //直接把音乐框隐藏
+        ////如果是移动端或微信环境，直接把音乐框隐藏
         $("#music_div").attr("style","display:none;");
     }
   }
 
-  // 判断浏览器函数
+  // 判断移动端
   function isMobile(){
    if(window.navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)) {
     return true; // 移动端
@@ -32,6 +32,14 @@
     return false; // PC端
    }
   }
+    //判断是否是微信环境
+    function isWxClient() {
+      var ua = navigator.userAgent.toLowerCase();
+      if (ua.match(/MicroMessenger/i) == "micromessenger") {
+        return true;
+      }
+      return false;
+    };
 
   show();
 })();
